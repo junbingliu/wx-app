2e2713ecd5499c3cd82a48048202e3da63e56eab
junbingliu <junbingliu@yeah.net>
2018-10-09 11:34:43 +0800

    Initial commit

---
+++ infoscape-officialWebSite-3/pages/home-page/home-page.js
@@ -0,0 +1,15 @@
+Page({
+  data: {
+    imgUrls: [
+      '../../image/01.jpg',
+      '../../image/02.jpg',
+      '../../image/03.jpg',
+      '../../image/04.jpg',
+      '../../image/05.jpg',
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000
+  },
+})
